9cab09b49b346a9bbf203f9f458210e49f188f5a
suxxsfe <suxxsfe@gmail.com>
2023-09-26 19:23:39 +0800

    feat(RulesOf6005.java): implemented mayUseCodeInAssignment()

--- ps0/src/rules/RulesOf6005.java
+++ ps0/src/rules/RulesOf6005.java
@@ -36,9 +36,15 @@ public class RulesOf6005 {
             boolean availableToOthers, boolean writtenAsCourseWork,
             boolean citingYourSource, boolean implementationRequired) {
         
-        // TODO: Fill in this method, then remove the exception
+        if(writtenByYourself){
+            return true;
+        }
+        if(availableToOthers && citingYourSource && !writtenAsCourseWork && !implementationRequired){
+            return true;
+        }
+        return false;
         
-        throw new RuntimeException("implement me!");
+//        throw new RuntimeException("implement me!");
     }
     
     /**
