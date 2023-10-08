9e49ed801f937a569cab556e6db07b03f93ba05d
suxxsfe <suxxsfe@gmail.com>
2023-10-08 19:10:50 +0800

    feat: implement Filter.java
    function writtenBy(), inTimespan(), containing()
    
    https://ocw.mit.edu/ans7870/6/6.005/s16/psets/ps1/#problem_2_filtering_lists_of_tweets

--- ps1/src/twitter/Filter.java
+++ ps1/src/twitter/Filter.java
@@ -4,6 +4,9 @@
 package twitter;
 
 import java.util.List;
+import java.util.*;
+
+import java.io.*;
 
 /**
  * Filter consists of methods that filter a list of tweets for those matching a
@@ -27,7 +30,15 @@ public class Filter {
      *         in the same order as in the input list.
      */
     public static List<Tweet> writtenBy(List<Tweet> tweets, String username) {
-        throw new RuntimeException("not implemented");
+        List<Tweet> tweetsWrittenBy = new ArrayList<>();
+        for(Tweet i: tweets){
+            if(i.getAuthor().equals(username)){
+                tweetsWrittenBy.add(i);
+            }
+        }
+        return tweetsWrittenBy;
+		
+//        throw new RuntimeException("not implemented");
     }
 
     /**
@@ -41,8 +52,50 @@ public class Filter {
      *         in the same order as in the input list.
      */
     public static List<Tweet> inTimespan(List<Tweet> tweets, Timespan timespan) {
-        throw new RuntimeException("not implemented");
+		List<Tweet> tweetsInTimespan = new ArrayList<>();
+		for(Tweet i: tweets){
+			int c1 = timespan.getStart().compareTo(i.getTimestamp());
+			int c2 = timespan.getEnd().compareTo(i.getTimestamp());
+			if((c1 == 0 || c1 == -1) && (c2 == 0 || c2 == 1)){
+				tweetsInTimespan.add(i);
+			}
+		}
+		return tweetsInTimespan;
+		
+//        throw new RuntimeException("not implemented");
     }
+	
+	/**
+	 * Check if text contain the wor
+	 * 
+	 * @param text
+	 * 			string text
+	 * @param word
+	 * 			a word with no spaces
+	 * @return return a boolean represent is the word in the text
+	 * 		   word should be bounded by space characters
+	 */
+	private static boolean isContaining(String text, String word){
+		text.toLowerCase();
+		word.toLowerCase();
+        
+//        System.out.println(text);
+//        System.out.println(word);
+        
+		for(int pos = text.indexOf(word); pos != -1; pos = text.indexOf(word, pos+word.length())){
+            
+//            System.out.printf("pos : %d\n",pos);
+            
+			if(pos != 0 && text.charAt(pos-1) != ' '){
+				continue;
+			}
+			if(pos != text.length() && text.charAt(pos+word.length()) != ' '){
+				continue;
+			}
+			return true;
+		}
+		return false;
+	}
 
     /**
      * Find tweets that contain certain words.
@@ -60,7 +113,23 @@ public class Filter {
      *         same order as in the input list.
      */
     public static List<Tweet> containing(List<Tweet> tweets, List<String> words) {
-        throw new RuntimeException("not implemented");
+		List<Tweet> tweetsContaining = new ArrayList<>();
+		for(Tweet i: tweets){
+			boolean contains = false;
+			for(String s: words){
+				if(isContaining(i.getText(),s)){
+					contains = true;
+					break;
+				}
+			}
+			if(contains){
+				tweetsContaining.add(i);
+			}
+		}
+		return tweetsContaining;
+		
+//        throw new RuntimeException("not implemented");
     }
 
 }
+
