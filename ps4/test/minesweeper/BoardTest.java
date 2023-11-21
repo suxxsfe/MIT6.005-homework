4e2f57a92b9191f88deb7af0732885acd7500d98
suxxsfe <suxxsfe@gmail.com>
2023-11-21 15:14:45 +0800

    test: test for Board ADT

--- ps4/test/minesweeper/BoardTest.java
+++ ps4/test/minesweeper/BoardTest.java
@@ -3,6 +3,7 @@
  */
 package minesweeper;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
@@ -17,6 +18,71 @@ public class BoardTest {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
     
-    // TODO: Tests
+    Board b = new Board(4, 3);
     
+    @Test
+    public void testInit(){
+        String s[] = b.getBoard();
+        
+        assertEquals("s[0] = ----", s[0], "----");
+        assertEquals("s[0] = ----", s[1], "----");
+        assertEquals("s[0] = ----", s[2], "----");
+    }
+    
+    @Test
+    public void testFlag(){
+        b.flag(0, 0);
+        b.flag(1, 2);
+        String s[] = b.getBoard();
+        
+        assertEquals("s[0] = F---", s[0], "F---");
+        assertEquals("s[1] = ----", s[1], "----");
+        assertEquals("s[2] = -F--", s[2], "-F--");
+    }
+    
+    @Test
+    public void testDeflag(){
+        b.flag(0, 0);
+        b.flag(1, 2);
+        b.deflag(0, 0);
+        String s[] = b.getBoard();
+        
+        assertEquals("s[0] = ----", s[0], "----");
+        assertEquals("s[1] = ----", s[1], "----");
+        assertEquals("s[2] = -F--", s[2], "-F--");
+    }
+    
+    @Test
+    public void testDigBomb(){
+        boolean bombs[][] = {
+            {false, false, true, true},
+            {false, true, true, true},
+            {false, false, true, false}};
+        
+        Board bb = new Board(4, 3, bombs);
+        boolean ret = bb.dig(1, 1);
+        String s[] = bb.getBoard();
+        
+        assertEquals("return true if dig a bomb", ret, true);
+        assertEquals("s[0] = ----", s[0], "----");
+        assertEquals("s[1] = -3--", s[1], "-3--");
+        assertEquals("s[2] = ----", s[2], "----");
+    }
+    
+    @Test
+    public void testDigBlank(){
+        boolean bombs[][] = {
+            {false, false, false, false},
+            {false, false, false, true},
+            {true, false, true, false}};
+        
+        Board bb = new Board(4, 3, bombs);
+        boolean ret = bb.dig(0, 0);
+        String s[] = bb.getBoard();
+        
+        assertEquals("return false if no bomb here", ret, false);
+        assertEquals("s[0] =   11", s[0], "  1-");
+        assertEquals("s[1] = 122-", s[1], "122-");
+        assertEquals("s[2] = ----", s[2], "----");
+    }
 }
