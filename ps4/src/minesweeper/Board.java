77da83aec67d85f4faaca90f612ca7d8e7bee467
suxxsfe <suxxsfe@gmail.com>
2023-11-21 15:15:00 +0800

    fix: Board(problem 2)
    https://ocw.mit.edu/ans7870/6/6.005/s16/psets/ps4/#problem_2_design_a_data_structure_for_minesweeper

--- ps4/src/minesweeper/Board.java
+++ ps4/src/minesweeper/Board.java
@@ -3,6 +3,8 @@
  */
 package minesweeper;
 
+import java.lang.Math;
+
 class Grid{
     private boolean containsBomb;
     private char status;
@@ -65,11 +67,28 @@ public class Board {
     static final int dNum = 8;
     static final int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
     static final int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+    static final double rate = 0.5;
     
     public Board(int _sizeX, int _sizeY){
         sizeX = _sizeX;
         sizeY = _sizeY;
         board = new Grid[sizeY][sizeX];
+        for(int i = 0; i < sizeX; i++){
+            for(int j = 0; j < sizeY; j++){
+                board[j][i] = new Grid(Math.random() <= rate);
+            }
+        }
+    }
+    
+    public Board(int _sizeX, int _sizeY, boolean[][] bombs){
+        sizeX = _sizeX;
+        sizeY = _sizeY;
+        board = new Grid[sizeY][sizeX];
+        for(int i = 0; i < sizeX; i++){
+            for(int j = 0; j < sizeY; j++){
+                board[j][i] = new Grid(bombs[j][i]);
+            }
+        }
     }
     
     private boolean checkGrid(int x, int y){
@@ -90,6 +109,7 @@ public class Board {
     public String[] getBoard(){
         String res[] = new String[sizeY];
         for(int j = 0; j < sizeY; j++){
+            res[j] = "";
             for(int i = 0; i < sizeX; i++){
                 char grid = board[j][i].getStatus();
                 res[j]+=(grid == ' ' ? countBombs(i, j) : grid);
@@ -114,10 +134,10 @@ public class Board {
         if(countBombs(x, y) != ' '){
             return;
         }
-        board[y][x].dig();
         for(int k = 0; k < dNum; k++){
             int newX = x+dx[k], newY = y+dy[k];
             if(checkGrid(newX, newY) && board[newY][newX].getStatus() != ' '){
+                board[newY][newX].dig();
                 digRec(newX, newY);
             }
         }
